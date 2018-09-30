8325e7fdf8f8c181daa6fb063ca3b3d75ca193b9
Kelvin Tan <kelvin.vins@gmail.com>
2018-09-30 18:21:40 +0800

    First Commit

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -9,17 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let iconImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "user"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let titleTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "24/7 support online"
+        textView.font = .boldSystemFont(ofSize: 16)
+        textView.textAlignment = .center
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.isSelectable = false
+        return textView
+    }()
+    
+    let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.text = "Come talk to our friendly representative who will always assist you with smile and never complain and always assists assist assist in everything. Come come come come come"
+        textView.textAlignment = .center
+        textView.textColor = .gray
+        textView.font = UIFont.systemFont(ofSize: 12)
+        textView.isSelectable = false
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(iconImageView)
+        view.addSubview(titleTextView)
+        view.addSubview(descriptionTextView)
+        configureLayout()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func configureLayout(){
+        iconImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        iconImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        iconImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        iconImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        titleTextView.topAnchor.constraint(equalTo: iconImageView.bottomAnchor, constant: 20).isActive = true
+        titleTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        titleTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        descriptionTextView.topAnchor.constraint(equalTo: titleTextView.bottomAnchor, constant: 10).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 10).isActive = true
     }
-
+    
+    
 
 }
 
